a3888208c8a60ef956e86cf2d3cd9181fe00d0c5
Heighties <heighties@gmail.com>
2022-11-08 23:14:05 +0100

    delete realisation function

--- src/pages/Realisations/index.jsx
+++ src/pages/Realisations/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useFetch } from '../../utils/hooks'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+// import { useFetch } from "../../utils/hooks";
+import { Link } from "react-router-dom";
 
 const StyledLink = styled(Link)`
   cursor: pointer;
@@ -13,16 +13,16 @@ const StyledLink = styled(Link)`
   justify-content: center;
   text-decoration: none;
   color: black;
-`
+`;
 
 const Wrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    gap: 4rem;
-    align-items: center;
-    padding-top: 5rem;
-`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 4rem;
+  align-items: center;
+  padding-top: 5rem;
+`;
 
 // const AddReal = styled.div`
 //     width: 150px;
@@ -35,60 +35,75 @@ const Wrapper = styled.div`
 const RealisationsSection = styled.div`
   display: flex;
   flex-direction: column-reverse;
-`
+`;
 
 const RealisationCard = styled.div`
   display: flex;
   flex-direction: row;
   gap: 15rem;
-`
+`;
 
-const RealisationTitle = styled.h2``
+const RealisationTitle = styled.h2``;
 
 const RealisationButton = styled.button`
   width: 8rem;
   height: 1.5rem;
   align-self: center;
-`
+`;
 
 const Buttons = styled.div`
   display: flex;
   gap: 20px;
-`
+`;
 
 function Realisations() {
-  const { data: realisationList, isLoading, error } = useFetch(
-    `http://localhost:8000/api/realisation`
-  )
-  
-if (isLoading) return <h1>LOADING...</h1>
+  const [realisation, setRealisation] = useState([]);
 
-if (error) {
-    return(
-        <h1>Error</h1> 
-      )
-  }
+  useEffect(() => {
+    getRealisation();
+  }, []);
 
-  console.log(realisationList)
+  const getRealisation = async () => {
+    let result = await fetch("http://localhost:8000/api/realisation");
+    result = await result.json();
+    setRealisation(result);
+  };
+
+  const deleteRealisation = async (id) => {
+    let result = await fetch(`http://localhost:8000/api/realisation/${id}`, {
+      method: "Delete",
+    });
+
+    result = await result.json();
+
+    if (result) {
+      alert("Realisation deleted");
+      getRealisation();
+    }
+  };
+
+  // console.log(realisationList);
 
   return (
     <Wrapper>
-      <StyledLink to={'/addrealisation'}>Add new</StyledLink>
+      <StyledLink to={"/addrealisation"}>Add new</StyledLink>
       <RealisationsSection>
-        {realisationList.map((realisation) => {
-          return(
-            <RealisationCard key={realisation._id}>
-              <RealisationTitle>{realisation.name}</RealisationTitle>
-                <Buttons>
-                  <StyledLink to={`/realisation/${realisation._id}`}>Modify</StyledLink>
-                  <RealisationButton>Delete</RealisationButton>
-                </Buttons>
+        {realisation.map((item) => {
+          return (
+            <RealisationCard key={item._id}>
+              <RealisationTitle>{item.name}</RealisationTitle>
+              <Buttons>
+                <StyledLink to={`/realisation/${item._id}`}>Modify</StyledLink>
+                <RealisationButton onClick={() => deleteRealisation(item._id)}>
+                  Delete
+                </RealisationButton>
+              </Buttons>
             </RealisationCard>
-          )
+          );
         })}
       </RealisationsSection>
     </Wrapper>
-  )
+  );
 }
 
-export default Realisations
+export default Realisations;
